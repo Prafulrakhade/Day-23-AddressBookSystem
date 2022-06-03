cdb9824bd9ea3cccfe590da7d8cc9f8229a5aad2
Praful <prafulrakhade02@gmail.com>
2022-06-03 11:54:04 +0530

    [Praful] Added UC8 Program File

--- Program.cs
+++ Program.cs
@@ -17,6 +17,8 @@ namespace AddressBookUsingCollectionLibrary
             {
                 Console.WriteLine("\nEnter 1 to Add person in AddressBook");
                 Console.WriteLine("Enter 2 to Display");
+                Console.WriteLine("Enter 3 to search the person by city");
+                Console.WriteLine("Enter 4 to search the person by State");
                 int choice = int.Parse(Console.ReadLine());
 
                 switch (choice)
@@ -26,6 +28,12 @@ namespace AddressBookUsingCollectionLibrary
                         break;
                     case 2:
                         records.ToPrint();
+                        break;
+                    case 3:
+                        records.CheckByCity();
+                        break;
+                    case 4:
+                        records.CheckByState();
                         break;
                     default:
                         Console.WriteLine("Pleasee select valid input");
